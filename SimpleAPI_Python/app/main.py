6446fd8684f198da5d7315f2b273d9d82c930129
Lakindu Hesara Perera <lakindupereraap@gmail.com>
2026-03-26 09:24:38 +0530

    feat: Implemented base FastAPI app to main

--- SimpleAPI_Python/app/main.py
+++ SimpleAPI_Python/app/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from simpleapi-python!")
+from app.db.database import db_manager
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # StartUp
+    await db_manager.initialize()
+    yield
+    # Shutdown (Ctrl + C)
+    await db_manager.close()
+
+
+app = FastAPI(
+    title="Simple Pins API",
+    description="Simple Pins API implementation with OAuth, for Week 2 Dev Onboarding",
+    version="0.1.0",
+    lifespan=lifespan,
+)
+
+
+@app.get("/")
+async def root():
+    return {"status": "online", "message": "API Active"}
